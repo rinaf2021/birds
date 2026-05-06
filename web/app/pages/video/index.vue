6ea2f3e28030abf1e255074ce850faa8bb5aac5e
Рина Ф <1@9797966.ru>
2026-05-06 16:59:35 +0300

    task-971 видео:
    - добавление видео в список
    - виды в отдельный файл
    - общие теги

--- web/app/pages/video/index.vue
+++ web/app/pages/video/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { UserProps } from '@nuxt/ui';
+import { amadinaFasciata, amadinaFasciataTags, estrildaMelpoda, estrildaMelpodaTags, estrildaTroglodytes, estrildaTroglodytesTags, geopeliaCuneata, geopeliaCuneataTags, stizopteraBichenovii, stizopteraBichenoviiTags, uraeginthusBengalus, uraeginthusBengalusTags } from '~/utils/species';
 
 const title = 'Видео'
 const description = ''
@@ -14,58 +14,147 @@ useSeoMeta({
 
 const items = ref([
 	{
+		title: 'Малыш красногорлой амадины, 1.5 недели)',
+		description: `${amadinaFasciataTags}`,
+		user: amadinaFasciata,
+		video: 'https://runtime.video.cloud.yandex.net/player/video/vplvgh37cdksht5lakv3?autoplay=0&mute=0'
+	},
+	{
+		title: 'Бриллиантовая горлица - 25 дней',
+		description: `Мамочка пошла отдыхать на месяц и теперь все дети на отцовских плечах!
+
+${geopeliaCuneataTags}`,
+		user: geopeliaCuneata,
+		video: 'https://runtime.video.cloud.yandex.net/player/video/vplvsz4ucmirinzowjph?autoplay=0&mute=0'
+	},
+	{
+		title: 'Когда ты не только крут, но и умен',
+		description: `${geopeliaCuneataTags}`,
+		user: geopeliaCuneata,
+		video: 'https://runtime.video.cloud.yandex.net/player/video/vplvt22dkt4pr2ljrwba?autoplay=0&mute=0'
+	},
+	{
+		title: 'Слеток бриллиантовой горлицы',
+		description: `А кто тут у нас крутышка?)))
+
+С вылетом!
+
+${geopeliaCuneataTags}`,
+		user: geopeliaCuneata,
+		video: 'https://runtime.video.cloud.yandex.net/player/video/vplvbxv2t7yhvp6f5tsg?autoplay=0&mute=0'
+	},
+	{
+		title: 'Птенцы бриллиантовой горлицы - 9 дней',
+		description: `9 и 8 дней)
+
+Растем как на дрожжах)
+
+${geopeliaCuneataTags}`,
+		user: geopeliaCuneata,
+		video: 'https://runtime.video.cloud.yandex.net/player/video/vplv6qewzinh2kehj73e?autoplay=0&mute=0'
+	},
+	{
+		title: 'Птенец красногорлой амадины',
+		description: `Вот такой малышок красногорлых амадин, прямиком из Африки (по нему видно). Ему неделька
+
+Одно кольцо профукал, поэтому номер 2, хоть и вылупился первым)
+
+${amadinaFasciataTags}`,
+		user: amadinaFasciata,
+		video: 'https://runtime.video.cloud.yandex.net/player/video/vplvyuwxssvwd2ibd3pp?autoplay=0&mute=0'
+	},
+	{
+		title: 'Красноухий астрильд оказался мальчиком',
+		description: `А помните я вам показывала видео с самочкой красноухого астрильда? Так вот - она мальчик)))
+
+Единственный вид гендерной неопределенности, который мне понятен
+
+${uraeginthusBengalusTags}`,
+		user: uraeginthusBengalus,
+		video: 'https://runtime.video.cloud.yandex.net/player/video/vplvma42xbzl7mbud34z?autoplay=0&mute=0'
+	},
+	{
+		title: 'Кормление птенцов бриллиантовой горлицы',
+		description: `${geopeliaCuneataTags}`,
+		user: geopeliaCuneata,
+		video: 'https://runtime.video.cloud.yandex.net/player/video/vplv4ywvceoysbfvjbkt?autoplay=0&mute=0'
+	},
+	{
+		title: 'Песня кольчатого астрильда',
+		description: `Только приехал, а уже девчонкам песни напевает)
+
+${stizopteraBichenoviiTags}`,
+		user: stizopteraBichenovii,
+		video: 'https://runtime.video.cloud.yandex.net/player/video/vplv7ogyydzkdqkd2uup?autoplay=0&mute=0'
+	},
+	{
+		title: 'Бриллиантовая горлица - счастливая мама',
+		description: `${geopeliaCuneataTags}`,
+		user: geopeliaCuneata,
+		video: 'https://runtime.video.cloud.yandex.net/player/video/vplvkmojik2uidzjgt3y?autoplay=0&mute=0'
+	},
+	{
+		title: 'Красногорлый амадин - воришка',
+		description: `С одной стороны - красть нехорошо. С другой - что не сделаешь ради благополучия будущих детей!
+
+${amadinaFasciataTags}`,
+		user: amadinaFasciata,
+		video: 'https://runtime.video.cloud.yandex.net/player/video/vplvkrvgp6kgke5hlhp7?autoplay=0&mute=0'
+	},
+	{
+		title: 'Бриллиантовые горлицы - смена караула',
+		description: `Бриллиатовые горлицы - крайне ответственные родители и за высиживание яиц отвечают оба родителя. Самочка сидит на них всю ночь, а самец - весь день. При этом ночью самец садится рядом с гнездом, чтобы контролировать окружающую обстановку.
+
+На видео показано как родители сменяют друг друга)
+
+${geopeliaCuneataTags}`,
+		user: geopeliaCuneata,
+		video: 'https://runtime.video.cloud.yandex.net/player/video/vplvwsn2rs3l7pcltdzc?autoplay=0&mute=0'
+	},
+	{
+		title: 'Песня самца оранжевощекого астрильда',
+		description: `Шикарная песня самца оранжевощекого астрильда - удивительно интересная для данного рода пернатых)
+
+${estrildaMelpodaTags}`,
+		user: estrildaMelpoda,
+		video: 'https://runtime.video.cloud.yandex.net/player/video/vplvp4o7g7izp6gz6l6s?autoplay=0&mute=0'
+	},
+	{
+		title: 'Парочка оранжевощеких астрильдов',
+		description: `Романтик пошел не по плану)
+
+${estrildaMelpodaTags}`,
+		user: estrildaMelpoda,
+		video: 'https://runtime.video.cloud.yandex.net/player/video/vplvuub5d444wusd2l2o?autoplay=0&mute=0'
+	},
+	{
+		title: 'Самочка красноухого астрильда',
+		description: `Есть небольшая склонность к разрушениям
+
+${uraeginthusBengalusTags}`,
+		user: uraeginthusBengalus,
+		video: 'https://runtime.video.cloud.yandex.net/player/video/vplvquf3nnv3vwicugq5?autoplay=0&mute=0'
+	},
+	{
 		title: 'Женская дружба - Красногорлая амадина',
 		description: `Я всегда считала, что самые стайные ребята среди амадин - это японские. Но как только я завела вторую самочку красногорлой мое мнение резко изменилось)
 
 Они постоянно вместе, даже гнездо выбирают одно на двоих) очень интересно, как они будут себя вести, когда парней к ним посажу - узнаем, насколько сильна женская дружба😁
 
-#красногорлаяамадина #amadinafasciata`,
-		orientation: 'horizontal' as const,
-		class: 'lg:col-span-3',
-		user: {
-			name: 'Красногорлая амадина',
-			description: 'Amadina Fasciata',
-			avatar: {
-				src: '/upload/birds/amadina_fasciata.webp',
-				alt: 'Amadina Fasciata'
-			},
-			size: '3xl' as UserProps['size'],
-		},
-		reverse: true,
+${amadinaFasciataTags}`,
+		user: amadinaFasciata,
 		video: 'https://runtime.video.cloud.yandex.net/player/video/vplvspuxqzwlueuubtd6?autoplay=0&mute=0'
 	},
 	{
-		title: 'Итак, вопрос на миллион: кто целуется лучше?)',
-		description: '',
-		orientation: 'horizontal' as const,
-		class: 'lg:col-span-3',
-		user: {
-			name: 'Серый астрильд',
-			description: 'Estrilda Troglodytes',
-			avatar: {
-				src: '/upload/birds/estrilda_troglodytes.webp',
-				alt: 'Estrilda Troglodytes'
-			},
-			size: '3xl' as UserProps['size'],
-		},
-		reverse: false,
+		title: `Итак, вопрос на миллион: кто целуется лучше?)`,
+		description: `${estrildaTroglodytesTags}`,
+		user: estrildaTroglodytes,
 		video: 'https://runtime.video.cloud.yandex.net/player/video/vplvr5klun4wfrwsixsq?autoplay=0&mute=0'
 	},
 	{
-		title: 'Научился красиво уходить в заказ',
-		description: '',
-		orientation: 'horizontal' as const,
-		class: 'lg:col-span-3',
-		user: {
-			name: 'Бриллиантовая горлица',
-			description: 'Geopelia cuneata',
-			avatar: {
-				src: '/upload/birds/geopelia_cuneata.webp',
-				alt: 'Geopelia cuneata'
-			},
-			size: '3xl' as UserProps['size'],
-		},
-		reverse: true,
+		title: `Научился красиво уходить в заказ`,
+		description: `${geopeliaCuneataTags}`,
+		user: geopeliaCuneata,
 		video: 'https://runtime.video.cloud.yandex.net/player/video/vplvet2gunsjacqq7gpv?autoplay=0&mute=0'
 	}
 ])
@@ -81,6 +170,9 @@ const items = ref([
 				v-for="(item, index) in items"
 				:key="index"
 				v-bind="item"
+				v-bind:reverse="(index % 2) === 0"
+				class="lg:col-span-3"
+				orientation="horizontal"
 			>
 				<iframe :src="item.video" class="w-full h-[450px]" style="background-color: #fff" allow="autoplay; encrypted-media; fullscreen; picture-in-picture; screen-wake-lock;" frameborder="0" allowfullscreen></iframe>
 				<template #description>
---
+++ web/app/utils/species.ts
@@ -0,0 +1,73 @@
+import type { UserProps } from "@nuxt/ui";
+
+export const amadinaFasciata = {
+	name: 'Красногорлая амадина',
+	description: 'Amadina Fasciata',
+	avatar: {
+		src: '/upload/birds/amadina_fasciata.webp',
+		alt: 'Amadina Fasciata'
+	},
+	size: '3xl' as UserProps['size'],
+};
+
+export const amadinaFasciataTags = '#красногорлаяамадина #amadinafasciata';
+
+export const geopeliaCuneata = {
+	name: 'Бриллиантовая горлица',
+	description: 'Geopelia cuneata',
+	avatar: {
+		src: '/upload/birds/geopelia_cuneata.webp',
+		alt: 'Geopelia cuneata'
+	},
+	size: '3xl' as UserProps['size'],
+}
+
+export const geopeliaCuneataTags = '#бриллиантоваягорлица #geopeliacuneata #diamondturtledove';
+
+export const estrildaTroglodytes = {
+	name: 'Серый астрильд',
+	description: 'Estrilda Troglodytes',
+	avatar: {
+		src: '/upload/birds/estrilda_troglodytes.webp',
+		alt: 'Estrilda Troglodytes'
+	},
+	size: '3xl' as UserProps['size'],
+}
+
+export const estrildaTroglodytesTags = '#серыйастрильд #estrildatroglodytes';
+
+export const estrildaMelpoda = {
+	name: 'Оранжевощекий астрильд',
+	description: 'Estrilda Melpoda',
+	avatar: {
+		src: '/upload/birds/estrilda_melpoda.webp',
+		alt: 'Estrilda Melpoda'
+	},
+	size: '3xl' as UserProps['size'],
+}
+
+export const estrildaMelpodaTags = '#оранжевощекийастрильд #estrildamelpoda';
+
+export const uraeginthusBengalus = {
+	name: 'Красноухий астрильд',
+	description: 'Uraenginthus Bengalus',
+	avatar: {
+		src: '/upload/birds/uraeginthus_bengalus.webp',
+		alt: 'Uraenginthus Bengalus'
+	},
+	size: '3xl' as UserProps['size'],
+}
+
+export const uraeginthusBengalusTags = '#красноухийастрильд #uraeginthusbengalus';
+
+export const stizopteraBichenovii = {
+	name: 'Кольчатый астрильд',
+	description: 'Stizoptera Bichenovii',
+	avatar: {
+		src: '/upload/birds/stizoptera_bichenovii.webp',
+		alt: 'Stizoptera Bichenovii'
+	},
+	size: '3xl' as UserProps['size'],
+}
+
+export const stizopteraBichenoviiTags = '#кольчатыйастрильд #stizopterabichenovii';
